e09b257dffd7a89aaa47f9692072a75e455818a6
sunny <55693813+sarathchandra706@users.noreply.github.com>
2020-04-18 00:35:12 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,43 @@
+import os
+
+from flask import Flask, session,render_template,request
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+
+    if (request.method=="POST"):
+        name = request.form.get("name")
+        pwd = request.form.get("pwd")
+        app.logger.info("name : " ,name)
+        app.logger.info("password : ",pwd)
+        return render_template("login.html",name=name)
+    else:
+        return render_template("register.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    return render_template("login.html")
+
+
